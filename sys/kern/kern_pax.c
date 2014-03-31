5a8f79dbbb32737e4503393170583d95c80712a6
Oliver Pinter <oliver.pntr@gmail.com>
2014-03-31 02:02:42 +0200

    PaX ASLR: fixed tunables in kern_pax.c
    Signed-off-by: Oliver Pinter <oliver.pntr@gmail.com>

--- sys/kern/kern_pax.c
+++ sys/kern/kern_pax.c
@@ -99,21 +99,21 @@ SYSCTL_PROC(_security_pax_aslr, OID_AUTO, mmap_len,
     NULL, 0, sysctl_pax_aslr_mmap, "I",
     "Number of bits randomized for mmap(2) calls. "
     "32 bit: [8,16] 64 bit: [16,32]");
-TUNABLE_INT("security.pax.aslr.mmap", &pax_aslr_mmap_len);
+TUNABLE_INT("security.pax.aslr.mmap_len", &pax_aslr_mmap_len);
 
 SYSCTL_PROC(_security_pax_aslr, OID_AUTO, stack_len,
     CTLTYPE_INT|CTLFLAG_RW|CTLFLAG_TUN|CTLFLAG_PRISON,
     NULL, 0, sysctl_pax_aslr_stack, "I",
     "Number of bits randomized for the stack. "
     "32 bit: [6,12] 64 bit: [12,21]");
-TUNABLE_INT("security.pax.aslr.stack", &pax_aslr_stack_len);
+TUNABLE_INT("security.pax.aslr.stack_len", &pax_aslr_stack_len);
 
 SYSCTL_PROC(_security_pax_aslr, OID_AUTO, exec_len,
     CTLTYPE_INT|CTLFLAG_RW|CTLFLAG_TUN|CTLFLAG_PRISON,
     NULL, 0, sysctl_pax_aslr_exec, "I",
     "Number of bits randomized for the PIE exec base. "
     "32 bit: [6,12] 64 bit: [12,21]");
-TUNABLE_INT("security.pax.aslr.stack", &pax_aslr_exec_len);
+TUNABLE_INT("security.pax.aslr.exec_len", &pax_aslr_exec_len);
 
 static int
 sysctl_pax_aslr_status(SYSCTL_HANDLER_ARGS)
