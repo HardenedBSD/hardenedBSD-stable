1407b8cd6c65659d3eb02b92cfd2c734d13de340
Shawn Webb <lattera@gmail.com>
2014-04-29 19:03:45 -0400

    PAX ASLR: Fix kernel panic when the execve call fails early on

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -507,7 +507,8 @@ interpret:
 	}
 
 #ifdef PAX_SEGVGUARD
-	error = pax_segvguard(curthread, imgp->vp, args->fname, 0);
+    if (!error)
+        error = pax_segvguard(curthread, imgp->vp, args->fname, 0);
 #endif
 
 	if (error) {
