e39b6bcd3483b92a99c51bae57721d485d7d9094
Oliver Pinter <oliver.pntr@gmail.com>
2014-05-16 05:44:00 +0200

    PAX: update license in pax.h
    Signed-off-by: Oliver Pinter <oliver.pntr@gmail.com>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -1,5 +1,6 @@
 /*-
- * Copyright (c) 2013, by Oliver Pinter <oliver.pntr at gmail.com>
+ * Copyright (c) 2013-2014, by Oliver Pinter <oliver.pntr at gmail.com>
+ * Copyright (c) 2014, by Shawn Webb <lattera at gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -7,8 +8,9 @@
  * are met:
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 2. The name of the developer may NOT be used to endorse or promote products
- *    derived from this software without specific prior written permission.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -23,8 +25,6 @@
  * SUCH DAMAGE.
  *
  * $FreeBSD$
- *
- * Enhancements made by Shawn "lattera" Webb under the direction of SoldierX.
  */
 
 #ifndef	__SYS_PAX_H
