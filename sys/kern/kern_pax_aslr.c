dd7f5ce4984df4a895a8ebc5a176a5105a6b4e8e
Oliver Pinter <op@hardenedbsd.org>
2014-09-22 22:48:21 +0200

    PAX ASLR: typo fix in CTR3
    Signed-off-by: Oliver Pinter <oliver.pntr@gmail.com>

--- sys/kern/kern_pax_aslr.c
+++ sys/kern/kern_pax_aslr.c
@@ -518,7 +518,7 @@ pax_aslr_active(struct proc *proc)
 		return (true);
 
 	CTR3(KTR_PAX, "%s: pid = %d p_pax = %x",
-	    proc->p_pid,__func__, flags);
+	    __func__, proc->p_pid, flags);
 
 	if ((flags & PAX_NOTE_ASLR) == PAX_NOTE_ASLR)
 		return (true);
