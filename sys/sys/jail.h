0be344217456ec591623e686b62c63d2d6e32b55
Shawn Webb <lattera@gmail.com>
2014-10-09 09:24:10 -0400

    Remove unneeded field from struct prison

--- sys/sys/jail.h
+++ sys/sys/jail.h
@@ -202,7 +202,6 @@ struct prison {
 	int              pr_pax_segvguard_maxcrashes;   /* (p) Maximum number of crashes before suspending application */
 	int		 pr_pax_map32_enabled;		/* (p) MAP_32BIT enabled (amd64 only) */
 	int		 pr_pax_proc_harden;		/* (p) Harden procfs */
-	void		 *pr_secfw_mac;			/* (p) secfw metadata */
 };
 
 struct prison_racct {
