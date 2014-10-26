lattera/freebsd

Phase 1 of migration of the per-jail hardening feature toggles to their own struct

sys/kern/kern_pax.c
@@ -203,35 +203,35 @@ pax_init_prison(struct prison *pr)
 	    __func__, pr->pr_name);
 
 #ifdef PAX_ASLR
-	pr->pr_pax_aslr_status = pax_aslr_status;
-	pr->pr_pax_aslr_debug = pax_aslr_debug;
-	pr->pr_pax_aslr_mmap_len = pax_aslr_mmap_len;
-	pr->pr_pax_aslr_stack_len = pax_aslr_stack_len;
-	pr->pr_pax_aslr_exec_len = pax_aslr_exec_len;
+	pr->pr_hardening.hr_pax_aslr_status = pax_aslr_status;
+	pr->pr_hardening.hr_pax_aslr_debug = pax_aslr_debug;
+	pr->pr_hardening.hr_pax_aslr_mmap_len = pax_aslr_mmap_len;
+	pr->pr_hardening.hr_pax_aslr_stack_len = pax_aslr_stack_len;
+	pr->pr_hardening.hr_pax_aslr_exec_len = pax_aslr_exec_len;
 
 #ifdef COMPAT_FREEBSD32
-	pr->pr_pax_aslr_compat_status = pax_aslr_compat_status;
-	pr->pr_pax_aslr_compat_mmap_len = pax_aslr_compat_mmap_len;
-	pr->pr_pax_aslr_compat_stack_len = pax_aslr_compat_stack_len;
-	pr->pr_pax_aslr_compat_exec_len = pax_aslr_compat_exec_len;
+	pr->pr_hardening.hr_pax_aslr_compat_status = pax_aslr_compat_status;
+	pr->pr_hardening.hr_pax_aslr_compat_mmap_len = pax_aslr_compat_mmap_len;
+	pr->pr_hardening.hr_pax_aslr_compat_stack_len = pax_aslr_compat_stack_len;
+	pr->pr_hardening.hr_pax_aslr_compat_exec_len = pax_aslr_compat_exec_len;
 #endif /* COMPAT_FREEBSD32 */
 #endif /* PAX_ASLR */
 
 #ifdef PAX_SEGVGUARD
-	pr->pr_pax_segvguard_status = pax_segvguard_status;
-	pr->pr_pax_segvguard_debug = pax_segvguard_debug;
-	pr->pr_pax_segvguard_expiry = pax_segvguard_expiry;
-	pr->pr_pax_segvguard_suspension = pax_segvguard_suspension;
-	pr->pr_pax_segvguard_maxcrashes = pax_segvguard_maxcrashes;
+	pr->pr_hardening.hr_pax_segvguard_status = pax_segvguard_status;
+	pr->pr_hardening.hr_pax_segvguard_debug = pax_segvguard_debug;
+	pr->pr_hardening.hr_pax_segvguard_expiry = pax_segvguard_expiry;
+	pr->pr_hardening.hr_pax_segvguard_suspension = pax_segvguard_suspension;
+	pr->pr_hardening.hr_pax_segvguard_maxcrashes = pax_segvguard_maxcrashes;
 #endif
 
 #if defined(PAX_HARDENING) && defined(MAP_32BIT)
-	pr->pr_pax_map32_enabled = pax_map32_enabled_global;
+	pr->pr_hardening.hr_pax_map32_enabled = pax_map32_enabled_global;
 #endif
 
 #ifdef PAX_HARDENING
-	pr->pr_pax_procfs_harden = pax_procfs_harden_global;
-	pr->pr_pax_mprotect_exec = pax_mprotect_exec_global;
+	pr->pr_hardening.hr_pax_procfs_harden = pax_procfs_harden_global;
+	pr->pr_hardening.hr_pax_mprotect_exec = pax_mprotect_exec_global;
 #endif
 
 	pr->pr_pax_set = 1;

sys/kern/kern_pax_hardening.c
@@ -160,7 +160,7 @@ sysctl_pax_allow_map32(SYSCTL_HANDLER_ARGS)
 
 	pr = pax_get_prison(req->td->td_proc);
 
-	val = (pr != NULL) ? pr->pr_pax_map32_enabled : pax_map32_enabled_global;
+	val = (pr != NULL) ? pr->pr_hardening.hr_pax_map32_enabled : pax_map32_enabled_global;
 	err = sysctl_handle_int(oidp, &val, sizeof(int), req);
 	if (err || (req->newptr == NULL))
 		return (err);
@@ -173,7 +173,7 @@ sysctl_pax_allow_map32(SYSCTL_HANDLER_ARGS)
 
 	if (pr != NULL) {
 		prison_lock(pr);
-		pr->pr_pax_map32_enabled = val;
+		pr->pr_hardening.hr_pax_map32_enabled = val;
 		prison_unlock(pr);
 	}
 
@@ -188,7 +188,7 @@ sysctl_pax_mprotect_exec(SYSCTL_HANDLER_ARGS)
 
 	pr = pax_get_prison(req->td->td_proc);
 
-	val = (pr != NULL) ? pr->pr_pax_mprotect_exec : pax_mprotect_exec_global;
+	val = (pr != NULL) ? pr->pr_hardening.hr_pax_mprotect_exec : pax_mprotect_exec_global;
 	err = sysctl_handle_int(oidp, &val, sizeof(int), req);
 	if (err || (req->newptr == NULL))
 		return (err);
@@ -201,7 +201,7 @@ sysctl_pax_mprotect_exec(SYSCTL_HANDLER_ARGS)
 
 	if (pr != NULL) {
 		prison_lock(pr);
-		pr->pr_pax_mprotect_exec = val;
+		pr->pr_hardening.hr_pax_mprotect_exec = val;
 		prison_unlock(pr);
 	}
 
@@ -216,7 +216,7 @@ sysctl_pax_procfs(SYSCTL_HANDLER_ARGS)
 
 	pr = pax_get_prison(req->td->td_proc);
 
-	val = (pr != NULL) ? pr->pr_pax_procfs_harden : pax_procfs_harden_global;
+	val = (pr != NULL) ? pr->pr_hardening.hr_pax_procfs_harden : pax_procfs_harden_global;
 	err = sysctl_handle_int(oidp, &val, sizeof(int), req);
 	if (err || (req->newptr == NULL))
 		return (err);
@@ -229,7 +229,7 @@ sysctl_pax_procfs(SYSCTL_HANDLER_ARGS)
 
 	if (pr != NULL) {
 		prison_lock(pr);
-		pr->pr_pax_procfs_harden = val;
+		pr->pr_hardening.hr_pax_procfs_harden = val;
 		prison_unlock(pr);
 	}
 
@@ -245,7 +245,7 @@ pax_map32_enabled(struct thread *td)
 	pr = pax_get_prison(td->td_proc);
 
 	if (pr != NULL && pr != &prison0)
-		return (pr->pr_pax_map32_enabled);
+		return (pr->pr_hardening.hr_pax_map32_enabled);
 
 	return (pax_map32_enabled_global);
 }
@@ -264,7 +264,7 @@ pax_procfs_harden(struct thread *td)
 	pr = pax_get_prison(td->td_proc);
 
 	if (pr != NULL && pr != &prison0)
-		return (pr->pr_pax_procfs_harden ? EPERM : 0);
+		return (pr->pr_hardening.hr_pax_procfs_harden ? EPERM : 0);
 
 	return (pax_procfs_harden_global ? EPERM : 0);
 }

HardenedBSD/hardenedBSD-stable

Conflicts:
	sys/kern/kern_pax.c
	sys/kern/kern_pax_hardening.c
	sys/sys/jail.h

sys/kern/kern_pax.c
@@ -203,34 +203,34 @@ pax_init_prison(struct prison *pr)
 	    __func__, pr->pr_name);
 
 #ifdef PAX_ASLR
-	pr->pr_pax_aslr_status = pax_aslr_status;
-	pr->pr_pax_aslr_debug = pax_aslr_debug;
-	pr->pr_pax_aslr_mmap_len = pax_aslr_mmap_len;
-	pr->pr_pax_aslr_stack_len = pax_aslr_stack_len;
-	pr->pr_pax_aslr_exec_len = pax_aslr_exec_len;
+	pr->pr_hardening.hr_pax_aslr_status = pax_aslr_status;
+	pr->pr_hardening.hr_pax_aslr_debug = pax_aslr_debug;
+	pr->pr_hardening.hr_pax_aslr_mmap_len = pax_aslr_mmap_len;
+	pr->pr_hardening.hr_pax_aslr_stack_len = pax_aslr_stack_len;
+	pr->pr_hardening.hr_pax_aslr_exec_len = pax_aslr_exec_len;
 
 #ifdef COMPAT_FREEBSD32
-	pr->pr_pax_aslr_compat_status = pax_aslr_compat_status;
-	pr->pr_pax_aslr_compat_mmap_len = pax_aslr_compat_mmap_len;
-	pr->pr_pax_aslr_compat_stack_len = pax_aslr_compat_stack_len;
-	pr->pr_pax_aslr_compat_exec_len = pax_aslr_compat_exec_len;
+	pr->pr_hardening.hr_pax_aslr_compat_status = pax_aslr_compat_status;
+	pr->pr_hardening.hr_pax_aslr_compat_mmap_len = pax_aslr_compat_mmap_len;
+	pr->pr_hardening.hr_pax_aslr_compat_stack_len = pax_aslr_compat_stack_len;
+	pr->pr_hardening.hr_pax_aslr_compat_exec_len = pax_aslr_compat_exec_len;
 #endif /* COMPAT_FREEBSD32 */
 #endif /* PAX_ASLR */
 
 #ifdef PAX_SEGVGUARD
-	pr->pr_pax_segvguard_status = pax_segvguard_status;
-	pr->pr_pax_segvguard_debug = pax_segvguard_debug;
-	pr->pr_pax_segvguard_expiry = pax_segvguard_expiry;
-	pr->pr_pax_segvguard_suspension = pax_segvguard_suspension;
-	pr->pr_pax_segvguard_maxcrashes = pax_segvguard_maxcrashes;
+	pr->pr_hardening.hr_pax_segvguard_status = pax_segvguard_status;
+	pr->pr_hardening.hr_pax_segvguard_debug = pax_segvguard_debug;
+	pr->pr_hardening.hr_pax_segvguard_expiry = pax_segvguard_expiry;
+	pr->pr_hardening.hr_pax_segvguard_suspension = pax_segvguard_suspension;
+	pr->pr_hardening.hr_pax_segvguard_maxcrashes = pax_segvguard_maxcrashes;
 #endif
 
 #ifdef PAX_HARDENING
 #ifdef MAP_32BIT
-	pr->pr_pax_map32_enabled = pax_map32_enabled_global;
+	pr->pr_hardening.hr_pax_map32_enabled = pax_map32_enabled_global;
 #endif
-	pr->pr_pax_procfs_harden = pax_procfs_harden_global;
-	pr->pr_pax_mprotect_exec_harden = pax_mprotect_exec_harden_global;
+	pr->pr_hardening.hr_pax_procfs_harden = pax_procfs_harden_global;
+	pr->pr_hardening.hr_pax_mprotect_exec = pax_mprotect_exec_harden_global;
 #endif
 
 	pr->pr_pax_set = 1;

sys/kern/kern_pax_hardening.c
@@ -161,7 +161,7 @@ sysctl_pax_allow_map32(SYSCTL_HANDLER_ARGS)
 
 	pr = pax_get_prison(req->td->td_proc);
 
-	val = (pr != NULL) ? pr->pr_pax_map32_enabled : pax_map32_enabled_global;
+	val = (pr != NULL) ? pr->pr_hardening.hr_pax_map32_enabled : pax_map32_enabled_global;
 	err = sysctl_handle_int(oidp, &val, sizeof(int), req);
 	if (err || (req->newptr == NULL))
 		return (err);
@@ -174,7 +174,7 @@ sysctl_pax_allow_map32(SYSCTL_HANDLER_ARGS)
 
 	if (pr != NULL) {
 		prison_lock(pr);
-		pr->pr_pax_map32_enabled = val;
+		pr->pr_hardening.hr_pax_map32_enabled = val;
 		prison_unlock(pr);
 	}
 
@@ -189,7 +189,7 @@ sysctl_pax_mprotect_exec(SYSCTL_HANDLER_ARGS)
 
 	pr = pax_get_prison(req->td->td_proc);
 
-	val = (pr != NULL) ? pr->pr_pax_mprotect_exec_harden : pax_mprotect_exec_harden_global;
+	val = (pr != NULL) ? pr->pr_hardening.hr_pax_mprotect_exec : pax_mprotect_exec_harden_global;
 	err = sysctl_handle_int(oidp, &val, sizeof(int), req);
 	if (err || (req->newptr == NULL))
 		return (err);
@@ -202,7 +202,7 @@ sysctl_pax_mprotect_exec(SYSCTL_HANDLER_ARGS)
 
 	if (pr != NULL) {
 		prison_lock(pr);
-		pr->pr_pax_mprotect_exec_harden = val;
+		pr->pr_hardening.hr_pax_mprotect_exec = val;
 		prison_unlock(pr);
 	}
 
@@ -217,7 +217,7 @@ sysctl_pax_procfs(SYSCTL_HANDLER_ARGS)
 
 	pr = pax_get_prison(req->td->td_proc);
 
-	val = (pr != NULL) ? pr->pr_pax_procfs_harden : pax_procfs_harden_global;
+	val = (pr != NULL) ? pr->pr_hardening.hr_pax_procfs_harden : pax_procfs_harden_global;
 	err = sysctl_handle_int(oidp, &val, sizeof(int), req);
 	if (err || (req->newptr == NULL))
 		return (err);
@@ -230,7 +230,7 @@ sysctl_pax_procfs(SYSCTL_HANDLER_ARGS)
 
 	if (pr != NULL) {
 		prison_lock(pr);
-		pr->pr_pax_procfs_harden = val;
+		pr->pr_hardening.hr_pax_procfs_harden = val;
 		prison_unlock(pr);
 	}
 
@@ -246,7 +246,7 @@ pax_map32_enabled(struct thread *td)
 	pr = pax_get_prison(td->td_proc);
 
 	if (pr != NULL && pr != &prison0)
-		return (pr->pr_pax_map32_enabled);
+		return (pr->pr_hardening.hr_pax_map32_enabled);
 
 	return (pax_map32_enabled_global);
 }
@@ -265,7 +265,7 @@ pax_procfs_harden(struct thread *td)
 	pr = pax_get_prison(td->td_proc);
 
 	if (pr != NULL && pr != &prison0)
-		return (pr->pr_pax_procfs_harden ? EPERM : 0);
+		return (pr->pr_hardening.hr_pax_procfs_harden ? EPERM : 0);
 
 	return (pax_procfs_harden_global ? EPERM : 0);
 }