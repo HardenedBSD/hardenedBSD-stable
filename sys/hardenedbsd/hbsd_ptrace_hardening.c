3283926c0849f135858f5206ad6d0462fca9f486
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2014-11-23 20:42:07 +0100

    HBSD: paxify ptrace hardening - part 2
    github-issue: #60 #62
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/hardenedbsd/hbsd_ptrace_hardening.c
+++ sys/hardenedbsd/hbsd_ptrace_hardening.c
@@ -57,24 +57,27 @@ static int sysctl_ptrace_hardening_status(SYSCTL_HANDLER_ARGS);
 static int sysctl_ptrace_hardening_gid(SYSCTL_HANDLER_ARGS);
 #endif
 
-static void ptrace_hardening_sysinit(void);
-
+#ifdef PAX_HARDENING
 int ptrace_hardening_status = PAX_FEATURE_SIMPLE_ENABLED;
-
+#else
+int ptrace_hardening_status = PAX_FEATURE_SIMPLE_DISABLED;
+#endif
 #ifdef PTRACE_HARDENING_GRP
 gid_t ptrace_hardening_allowed_gid = PTRACE_HARDENING_GRP;
 #endif
 
-TUNABLE_INT("hardening.ptrace.status", &ptrace_hardening_status);
+TUNABLE_INT("hardening.ptrace_hardening.status",
+    &ptrace_hardening_status);
 #ifdef PTRACE_HARDENING_GRP
-TUNABLE_INT("hardening.ptrace.allowed_gid", &ptrace_hardening_allowed_gid);
+TUNABLE_INT("hardening.ptrace_hardening.allowed_gid",
+    &ptrace_hardening_allowed_gid);
 #endif
 
 #ifdef PAX_SYSCTLS
-SYSCTL_NODE(_hardening, OID_AUTO, ptrace, CTLFLAG_RD, 0,
+SYSCTL_NODE(_hardening, OID_AUTO, ptrace_hardening, CTLFLAG_RD, 0,
     "PTrace settings.");
 
-SYSCTL_PROC(_hardening_ptrace, OID_AUTO, status,
+SYSCTL_PROC(_hardening_ptrace_hardening, OID_AUTO, status,
     CTLTYPE_INT|CTLFLAG_RWTUN|CTLFLAG_PRISON|CTLFLAG_SECURE,
     NULL, 0, sysctl_ptrace_hardening_status, "I",
     "Restrictions status. "
@@ -82,12 +85,40 @@ SYSCTL_PROC(_hardening_ptrace, OID_AUTO, status,
     "1 - enabled");
 
 #ifdef PTRACE_HARDENING_GRP
-SYSCTL_PROC(_hardening_ptrace, OID_AUTO, allowed_gid,
+SYSCTL_PROC(_hardening_ptrace_hardening, OID_AUTO, allowed_gid,
     CTLTYPE_ULONG|CTLFLAG_RWTUN|CTLFLAG_PRISON|CTLFLAG_SECURE,
     NULL, 0, sysctl_ptrace_hardening_gid, "LU",
     "Allowed gid");
 #endif
 #endif
+
+static void
+ptrace_hardening_sysinit(void)
+{
+
+	switch (ptrace_hardening_status) {
+	case PAX_FEATURE_SIMPLE_DISABLED:
+	case PAX_FEATURE_SIMPLE_ENABLED:
+		break;
+	default:
+		printf("[PAX HARDENING] WARNING, invalid settings in loader.conf!"
+		    " (hardening.ptrace_hardening.status= %d)\n",
+		    ptrace_hardening_status);
+		ptrace_hardening_status = PAX_FEATURE_SIMPLE_ENABLED;
+	}
+	printf("[PAX HARDENING] ptrace hardening status: %s\n",
+	    pax_status_simple_str[ptrace_hardening_status]);
+
+#ifdef PTRACE_HARDENING_GRP
+	if (val < 0 || val > GID_MAX) {
+		panic("[PAX HARDENING] ptrace hardening\n"
+		    "hardening.ptrace_hardening.allowed_gid not in range!\n");
+	}
+	printf("[PAX HARDENING] ptrace hardening allowed gid : %d\n",
+	    ptrace_hardening_allowed_gid);
+#endif
+}
+SYSINIT(ptrace_hardening, SI_SUB_PAX, SI_ORDER_THIRD, ptrace_hardening_sysinit, NULL);
 
 #ifdef PAX_SYSCTLS
 int
@@ -138,17 +169,18 @@ int
 ptrace_hardening(struct thread *td, u_int ptrace_hardening_flag)
 {
 
-	if (!ptrace_hardening_status)
+	if (ptrace_hardening_status == PAX_FEATURE_SIMPLE_DISABLED)
 		return (0);
 
 	uid_t uid = td->td_ucred->cr_ruid;
 	gid_t gid = td->td_ucred->cr_rgid;
+	if ((uid != 0)
 #ifdef PTRACE_HARDENING_GRP
-	if (uid && (ptrace_hardening_allowed_gid &&
-	    gid != ptrace_hardening_allowed_gid)) {
-#else
-	if (uid) {
+	    // XXXOP: wheel group can't specified ?!
+	    && ((ptrace_hardening_allowed_gid != 0) &&
+	    gid != ptrace_hardening_allowed_gid)
 #endif
+	    ) {
 
 		pax_log_ptrace_hardening(td->td_proc, __func__,
 		    "forbidden ptrace call attempt "
@@ -160,15 +192,4 @@ ptrace_hardening(struct thread *td, u_int ptrace_hardening_flag)
 	return (0);
 }
 
-static void
-ptrace_hardening_sysinit(void)
-{
-	printf("[PTRACE HARDENING] status : %d\n", ptrace_hardening_status);
-
-#ifdef PTRACE_HARDENING_GRP
-	printf("[PTRACE HARDENING] allowed gid : %d\n",
-	    ptrace_hardening_allowed_gid);
-#endif
-}
-SYSINIT(ptrace, SI_SUB_PTRACE_HARDENING, SI_ORDER_FIRST, ptrace_hardening_sysinit, NULL);
 
