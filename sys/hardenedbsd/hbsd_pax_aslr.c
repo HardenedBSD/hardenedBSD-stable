13d364da2232253e81256b063653283c8dfedbb7
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2014-11-24 14:25:37 +0100

    HBSD ASLR: remove noisy log, when ASLR is opt-in
    github-issue: #60
    Signed-off-by: Oliver Pinter <oliver.pntr@gmail.com>

--- sys/hardenedbsd/hbsd_pax_aslr.c
+++ sys/hardenedbsd/hbsd_pax_aslr.c
@@ -776,10 +776,6 @@ pax_aslr_setup_flags(struct image_params *imgp, u_int mode)
 		} else {
 			flags &= ~PAX_NOTE_ASLR;
 			flags |= PAX_NOTE_NOASLR;
-			pax_log_aslr(imgp->proc, __func__,
-	"ASLR is opt-in, and executable don't have enabled ASLR!\n");
-			pax_ulog_aslr(NULL,
-	"ASLR is opt-in, and executable don't have enabled ASLR!\n");
 		}
 
 		return (flags);
