5e89a99d58e77405308085385ed9ddfdd8318b09
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2014-11-29 02:20:04 +0100

    HBSD: added myself to ptrace's copyright
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/hardenedbsd/hbsd_ptrace_hardening.c
+++ sys/hardenedbsd/hbsd_ptrace_hardening.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2014, by David Carlier <devnexen at gmail.com>
+ * Copyright (c) 2014, by Oliver Pinter <oliver.pinter@hardenedbsd.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
