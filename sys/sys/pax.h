fd5db3652fa0f4accc8aa868f6caa6e26b813b6f
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2014-11-29 15:11:20 +0100

    HBSD NOEXEC: remove old basic version of mprotect hardening
    github-issue: #37
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -53,7 +53,6 @@ struct hardening_features {
 	int	 hr_pax_mprotect_status;	/* (p) Enforce W^X mappings */
 	int	 hr_pax_map32_enabled;		/* (p) MAP_32BIT enabled (amd64 only) */
 	int	 hr_pax_procfs_harden;		/* (p) Harden procfs */
-	int	 hr_pax_mprotect_exec;		/* (p) Disallow setting exec bit on non-exec mappings */
 	int	 hr_pax_ptrace_hardening_status;	/* (p) Disallow unprivileged ptrace */
 	gid_t	 hr_pax_ptrace_hardening_gid;	/* (p) Allowed ptrace users group */
 };
