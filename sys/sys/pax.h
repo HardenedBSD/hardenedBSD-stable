f5a9ffbcc4f619c48ba4c4df7766fdbb9dbdf056
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2014-11-29 17:56:06 +0100

    HBSD NOEXEC: change pax_{pageexec,segvguard} parameter type
    github-issue: #37
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -164,7 +164,7 @@ void pax_pageexec_init_prison(struct prison *pr);
 #define	pax_pageexec_init_prison(pr)	do {} while (0)
 #endif
 u_int pax_pageexec_setup_flags(struct image_params *imgp, u_int mode);
-void pax_pageexec(struct proc *p, vm_prot_t *prot, vm_prot_t *maxprot);
+void pax_pageexec(struct proc *p, int *prot, vm_prot_t *maxprot);
 int pax_mprotect_active(struct proc *p);
 #ifdef PAX_MPROTECT
 void pax_mprotect_init_prison(struct prison *pr);
@@ -172,7 +172,7 @@ void pax_mprotect_init_prison(struct prison *pr);
 #define	pax_mprotect_init_prison(pr)	do {} while (0)
 #endif
 u_int pax_mprotect_setup_flags(struct image_params *imgp, u_int mode);
-void pax_mprotect(struct proc *p, vm_prot_t *prot, vm_prot_t *maxprot);
+void pax_mprotect(struct proc *p, int *prot, vm_prot_t *maxprot);
 int pax_mprotect_enforce(struct proc *p, vm_prot_t old_prot, vm_prot_t new_prot);
 
 /*
