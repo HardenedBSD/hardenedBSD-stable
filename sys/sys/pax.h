156dd2184993e3ce5e938da070574459c677c28f
Oliver Pinter <oliver.pntr@gmail.com>
2014-12-06 20:28:26 +0100

    HBSD: bump __HardenedBSD_version after aout, ibcs2 and svr4 image activator removal
    Signed-off-by: Oliver Pinter <oliver.pinetr@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	__SYS_PAX_H
 #define	__SYS_PAX_H
 
-#define	__HardenedBSD_version	6
+#define	__HardenedBSD_version	7
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
