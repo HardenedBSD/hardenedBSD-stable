7496094152e837924e519e87ab2ff71caf048090
Shawn Webb <shawn.webb@hardenedbsd.org>
2014-12-17 15:12:19 -0500

    Create MAC callbacks for jail creation/deletion github-issue: #91
    Bump HardenedBSD version number due to MAC ABI change.
    
    (cherry picked from commit b734eb479f82c93fb5ce93a66f49672ca059ebd2)
    Signed-off-by: Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	__SYS_PAX_H
 #define	__SYS_PAX_H
 
-#define	__HardenedBSD_version	8
+#define	__HardenedBSD_version	9
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
