f8b9300b7e98e5bcd3151f9b99f76dbb309956d5
Oliver Pinter <op@hardenedbsd.org>
2015-01-09 02:09:07 +0100

    HBSD: bump _HardenedBSD_version after upstream OpenSSL upgrade
    Notes: I forgot to bump the version, after boot hardening, so this
    is really v11.
    https://github.com/HardenedBSD/hardenedBSD-stable/releases/tag/HardenedBSD-11-CURRENT-v10
    
    Signed-off-by: Oliver Pinter <oliver.pntr@gmail.com>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	__SYS_PAX_H
 #define	__SYS_PAX_H
 
-#define	__HardenedBSD_version	9
+#define	__HardenedBSD_version	11
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
