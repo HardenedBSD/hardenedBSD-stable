d0285a9827a88bb07965649c727279fe3a69a6e8
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-04-17 02:05:33 +0200

    HBSD: Bump __HardenedBSD_version to 20 after stack randomzation change
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	19
+#define	__HardenedBSD_version	20
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
