52296146e9daae1e7bd8ced20c91448d49e469f2
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-05-01 23:52:40 +0200

    HBSD: no functional change, only reorder the included files
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>
    github-issue: #128

--- sys/compat/ia32/ia32_sysvec.c
+++ sys/compat/ia32/ia32_sysvec.c
@@ -43,8 +43,8 @@ __FBSDID("$FreeBSD$");
 #include <sys/mutex.h>
 #include <sys/mman.h>
 #include <sys/namei.h>
-#include <sys/pioctl.h>
 #include <sys/pax.h>
+#include <sys/pioctl.h>
 #include <sys/proc.h>
 #include <sys/procfs.h>
 #include <sys/resourcevar.h>
