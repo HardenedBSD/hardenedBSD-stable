0b206bedc47955175f55929dcb6584e65dd32475
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-06-07 01:39:15 +0200

    HBSD: bump __HardenedBSD_version to 25 after fixed expression
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>
    (cherry picked from commit 5bed9fc3cd78b3f6be131da50a1b598382cb42e6)
    (cherry picked from commit 86c94ab5235c87125a5f262f2d42ab5e96068770)

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	24
+#define	__HardenedBSD_version	25
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
