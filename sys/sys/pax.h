ac32d0b3c1b9a2c764b036b18aaf5974e86cfad4
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-07-01 03:11:36 +0200

    HBSD: bump __HardenedBSD_version to 26 after ASLR rewrite
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	25
+#define	__HardenedBSD_version	26
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
