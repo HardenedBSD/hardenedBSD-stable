19f6ffa3b0593b851b61a183447e75ade7a71a73
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-07-15 02:37:58 +0200

    HBSD: I have forgotten to bump __HardenedBSD_version to 28 after vdso random fix, so do it now
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	27
+#define	__HardenedBSD_version	28
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
