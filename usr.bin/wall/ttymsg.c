39c8333c21682812a52298403dc76f77ab8e7abc
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-07-30 23:05:28 +0200

    HBSD: fix buffer overflow in syslogd, wall and talkd
    Found-with: FORTIFY_SOURCE
    MFC-to: HardenedBSD 10-STABLE
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>
    CC: Pedro Giffuni <pfg@FreeBSD.org>
    (cherry picked from commit 6e092f69491171b2a35c7ef3ae8ef99094b5fd59)

--- usr.bin/wall/ttymsg.c
+++ usr.bin/wall/ttymsg.c
@@ -71,8 +71,8 @@ ttymsg(struct iovec *iov, int iovcnt, const char *line, int tmout)
 	if (iovcnt > (int)(sizeof(localiov) / sizeof(localiov[0])))
 		return ("too many iov's (change code in wall/ttymsg.c)");
 
+	strlcat(device, line, sizeof(device));
 	p = device + sizeof(_PATH_DEV) - 1;
-	strlcpy(p, line, sizeof(device));
 	if (strncmp(p, "pts/", 4) == 0)
 		p += 4;
 	if (strchr(p, '/') != NULL) {
