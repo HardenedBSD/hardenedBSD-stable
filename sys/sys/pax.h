3e87bc41f0db34d5ede2f8118167dafd1c80bc4a
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-08-23 01:20:56 +0200

    HBSD: bumo __HardenedBSD_version to 29 after pkg bootstrap fix
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	28
+#define	__HardenedBSD_version	29
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
