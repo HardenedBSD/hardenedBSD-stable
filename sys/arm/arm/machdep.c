20ab9dda7dc673a9e5f0bcd7a3576df0c14a4aa2
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-09-03 19:34:30 +0200

    HBSD: style wrap long line in sys/arm/arm/machdep.c
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>
    (cherry picked from commit 2b37ef0d295af06ffa689d00331b59cb1fc52ad1)
    (cherry picked from commit 639ff8d9717d21072ec28aaa26360a322abc07c4)

--- sys/arm/arm/machdep.c
+++ sys/arm/arm/machdep.c
@@ -280,7 +280,8 @@ sendsig(catcher, ksi, mask)
 	tf->tf_r5 = (register_t)&fp->sf_uc;
 	tf->tf_pc = (register_t)catcher;
 	tf->tf_usr_sp = (register_t)fp;
-	tf->tf_usr_lr = (register_t)(p->p_psstrings - *(p->p_sysent->sv_szsigcode));
+	tf->tf_usr_lr = (register_t)(p->p_psstrings -
+	    *(p->p_sysent->sv_szsigcode));
 
 	CTR3(KTR_SIG, "sendsig: return td=%p pc=%#x sp=%#x", td, tf->tf_usr_lr,
 	    tf->tf_usr_sp);
