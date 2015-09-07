734af278d58a1492cfb711d407da7b6796953de4
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-09-07 16:09:23 +0200

    HBSD: bump __HardenedBSD_version to 31 after core changes and DISALLOWMAP32BIT rewrite
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	30
+#define	__HardenedBSD_version	31
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
