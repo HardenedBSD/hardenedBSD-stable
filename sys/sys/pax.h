5093a525cf76e11d022b7aebb284d3f71f31c918
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-11-10 22:14:28 +0100

    HBSD NOEXEC: refactor and fix inversed expression
    Note: before the previous commit (9f69f834b2a48e5dbdd0e8e25a79b606772613d2),
    this API was never used, so this fix isn't impilcate a security issues.
    
    github-issue: #37
    MFC-to: 10-STABLE
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>
    (cherry picked from commit eb32afe97b0b76ab1dd006d65843372701cf07bd)
    (cherry picked from commit a353a3769223699a34c859b67ee96da17b8c0e1e)

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -69,6 +69,7 @@ struct prison;
 struct thread;
 struct proc;
 struct vnode;
+struct vm_map_t;
 struct vm_offset_t;
 
 /*
@@ -185,7 +186,7 @@ u_int pax_pageexec_setup_flags(struct image_params *imgp, struct thread *td, uin
 u_int pax_mprotect_setup_flags(struct image_params *imgp, struct thread *td, uint32_t mode);
 void pax_pageexec(struct proc *p, vm_prot_t *prot, vm_prot_t *maxprot);
 void pax_mprotect(struct proc *p, vm_prot_t *prot, vm_prot_t *maxprot);
-int pax_mprotect_enforce(struct proc *p, vm_prot_t old_prot, vm_prot_t new_prot);
+int pax_mprotect_enforce(struct proc *p, vm_map_t map, vm_prot_t old_prot, vm_prot_t new_prot);
 
 /*
  * Hardening related functions
