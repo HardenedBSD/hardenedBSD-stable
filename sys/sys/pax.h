8ba10d49269111e4642693d179feec0cda85b6cb
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-11-23 20:38:28 +0100

    HBSD: bump __HardenedBSD_version to 35 after recent VDSO related proc structure changes
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	34
+#define	__HardenedBSD_version	35
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
