d2fd870f52a3e6c82aa0f6738ba443cae4d8973c
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-12-18 22:48:25 +0100

    HBSD: bump __HardenedBSD_version to 38 after MAP_32BIT randmization
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>
    (cherry picked from commit b82066b862a13cb37c107551718b1483b634ee58)

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	37
+#define	__HardenedBSD_version	38
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
