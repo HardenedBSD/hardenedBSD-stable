7ec3e047b39b09f482c23f0c2ae9cdc86e6d7dfc
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2016-01-09 20:42:46 +0100

    HBSD: fix merge conflict after linuxulator64 MFC in sys/i386/i386/elf_machdep.c
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/i386/i386/elf_machdep.c
+++ sys/i386/i386/elf_machdep.c
@@ -90,11 +90,8 @@ struct sysentvec elf32_freebsd_sysvec = {
 	.sv_shared_page_base = SHAREDPAGE,
 	.sv_shared_page_len = PAGE_SIZE,
 	.sv_schedtail	= NULL,
-<<<<<<< HEAD
-	.sv_pax_aslr_init = pax_aslr_init_vmspace,
-=======
 	.sv_thread_detach = NULL,
->>>>>>> freebsd/stable/10
+	.sv_pax_aslr_init = pax_aslr_init_vmspace,
 };
 INIT_SYSENTVEC(elf32_sysvec, &elf32_freebsd_sysvec);
 
