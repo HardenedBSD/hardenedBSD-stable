610095359c42a0c2d5f4aae12d0c7aee87fed5aa
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2016-01-09 20:45:36 +0100

    HBSD: fix merge conflict after linuxulator64 MFC in sys/arm/arm/elf_machdep.c
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/arm/arm/elf_machdep.c
+++ sys/arm/arm/elf_machdep.c
@@ -82,11 +82,8 @@ struct sysentvec elf32_freebsd_sysvec = {
 	.sv_fetch_syscall_args = cpu_fetch_syscall_args,
 	.sv_syscallnames = syscallnames,
 	.sv_schedtail	= NULL,
-<<<<<<< HEAD
-	.sv_pax_aslr_init = pax_aslr_init_vmspace,
-=======
 	.sv_thread_detach = NULL,
->>>>>>> freebsd/stable/10
+	.sv_pax_aslr_init = pax_aslr_init_vmspace,
 };
 
 static Elf32_Brandinfo freebsd_brand_info = {
