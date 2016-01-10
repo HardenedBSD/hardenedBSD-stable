124fffb2b35364a7d0153f4d2677ac47682e65ab
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2016-01-10 22:22:18 +0100

    HBSD: fix merge conflict after e706df7b9aabea4208d9f836f591eae7b4424987 in sys/amd64/linux/linux_sysvec.c
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/amd64/linux/linux_sysvec.c
+++ sys/amd64/linux/linux_sysvec.c
@@ -829,11 +829,8 @@ struct sysentvec elf_linux_sysvec = {
 	.sv_shared_page_len = PAGE_SIZE,
 	.sv_schedtail	= linux_schedtail,
 	.sv_thread_detach = linux_thread_detach,
-<<<<<<< HEAD
-	.sv_pax_aslr_init = pax_aslr_init_vmspace,
-=======
 	.sv_trap	= linux_vsyscall,
->>>>>>> freebsd/master
+	.sv_pax_aslr_init = pax_aslr_init_vmspace,
 };
 
 static void
