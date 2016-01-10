def98aebb6b8fd732db41913058bac5fbb0b544d
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2016-01-10 22:21:27 +0100

    HBSD: fix merge conflict after e706df7b9aabea4208d9f836f591eae7b4424987 in sys/arm/arm/elf_machdep.c
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/arm/arm/elf_machdep.c
+++ sys/arm/arm/elf_machdep.c
@@ -89,11 +89,8 @@ struct sysentvec elf32_freebsd_sysvec = {
 	.sv_shared_page_len = PAGE_SIZE,
 	.sv_schedtail	= NULL,
 	.sv_thread_detach = NULL,
-<<<<<<< HEAD
-	.sv_pax_aslr_init = pax_aslr_init_vmspace,
-=======
 	.sv_trap	= NULL,
->>>>>>> freebsd/master
+	.sv_pax_aslr_init = pax_aslr_init_vmspace,
 };
 INIT_SYSENTVEC(elf32_sysvec, &elf32_freebsd_sysvec);
 
