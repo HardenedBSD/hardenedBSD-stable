011b0f83de6b68e8097230084d45847631374ee5
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2016-01-12 11:59:48 +0100

    HBSD: change UFS type to UFS2 in make-uefi-memstick.sh too
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>
    github-issue: #179

--- release/amd64/make-uefi-memstick.sh
+++ release/amd64/make-uefi-memstick.sh
@@ -30,7 +30,7 @@ fi
 
 echo '/dev/ufs/HardenedBSD_Install / ufs ro,noatime 1 1' > ${1}/etc/fstab
 echo 'root_rw_mount="NO"' > ${1}/etc/rc.conf.local
-makefs -B little -o label=HardenedBSD_Install ${2}.part ${1}
+makefs -B little -o label=HardenedBSD_Install,version=2 ${2}.part ${1}
 if [ $? -ne 0 ]; then
 	echo "makefs failed"
 	exit 1
