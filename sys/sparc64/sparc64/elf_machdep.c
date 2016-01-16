6bf8cea5fb6a5de7ea5643db3fe71a08ae4314fb
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2016-01-16 13:56:02 +0100

    HBSD: fix merge conflict after ed08737097eb396742e2825609c2449687d3a944 commit in sys/sparc64/sparc64/elf_machdep.c
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sparc64/sparc64/elf_machdep.c
+++ sys/sparc64/sparc64/elf_machdep.c
@@ -91,11 +91,8 @@ static struct sysentvec elf64_freebsd_sysvec = {
 	.sv_syscallnames = syscallnames,
 	.sv_schedtail	= NULL,
 	.sv_thread_detach = NULL,
-<<<<<<< HEAD
-	.sv_pax_aslr_init = pax_aslr_init_vmspace,
-=======
 	.sv_trap	= NULL,
->>>>>>> freebsd/stable/10
+	.sv_pax_aslr_init = pax_aslr_init_vmspace,
 };
 
 static Elf64_Brandinfo freebsd_brand_info = {
