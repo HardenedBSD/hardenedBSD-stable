91f97d2456e5d8ebddc70a7f25483d00781d4c89
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2016-01-23 23:12:52 +0100

    HBSD: bump __HardenedBSD_version to 40 after HBSD API changes
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	39UL
+#define	__HardenedBSD_version	40UL
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hardening_features {
