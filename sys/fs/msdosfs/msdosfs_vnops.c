3a87a2e53a734b67caf4c8fbbc4c7194d92a229e
pfg <pfg@FreeBSD.org>
2016-02-07 15:36:16 +0000

    HBSD MFC: msdosfs_rename: yet another unused value.
    As with r295355, it seems to be left over from a cleanup
    in r33548. The code is not in NetBSD either.
    
    Thanks to bde for checking out the history.
    
    (cherry picked from commit b42dfac65547da9f8e3778f95d4af3448f362dda)
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/fs/msdosfs/msdosfs_vnops.c
+++ sys/fs/msdosfs/msdosfs_vnops.c
@@ -1018,12 +1018,9 @@ msdosfs_rename(ap)
 	int error;
 	u_long cn, pcl;
 	daddr_t bn;
-	struct denode *fddep;	/* from file's parent directory	 */
 	struct msdosfsmount *pmp;
 	struct direntry *dotdotp;
 	struct buf *bp;
-
-	fddep = VTODE(ap->a_fdvp);
 
 	pmp = VFSTOMSDOSFS(fdvp->v_mount);
 
