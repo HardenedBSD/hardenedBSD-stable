fdc87cf91a26ce9c54251556f271914945d005b7
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2016-02-13 18:58:50 +0100

    HBSD: bump __HardenedBSD_version to 41 after recent changes
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>
    (cherry picked from commit 77c89333d3e03d9e1cea7cedbd44a5e30e55d3e1)

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	40UL
+#define	__HardenedBSD_version	41UL
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hbsd_features {
