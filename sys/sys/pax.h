29afa4160221ff2278af57ed3a0d98729ef4699e
Shawn Webb <shawn.webb@hardenedbsd.org>
2016-02-25 09:28:27 -0500

    HBSD: Bump HardenedBSD version to 42 after rtld noexec change
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    MFC-to:		10-STABLE
    MFC-with:	1ae50df934506defc128f3e52a0698f0fbdc2368
    (cherry picked from commit 2fcb2ba787380cbca742effcffd3909ce79eba83)

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	41UL
+#define	__HardenedBSD_version	42UL
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hbsd_features {
