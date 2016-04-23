e7ed61521be668c62f65fdbc55d4b7531a05ff76
Shawn Webb <shawn.webb@hardenedbsd.org>
2016-04-23 14:24:52 -0400

    HBSD: Bump __HardenedBSD_version to 46.
    Introducing RELRO + BIND_NOW for base requires HardenedBSD version
    number bump.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	45UL
+#define	__HardenedBSD_version	46UL
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hbsd_features {
