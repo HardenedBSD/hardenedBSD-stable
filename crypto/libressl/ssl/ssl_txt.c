73b2e30208340039b2f7be7b0490651d380ad87d
Bernard Spil <brnrd@FreeBSD.org>
2016-08-03 21:56:27 +0200

    HBSD: Update LibreSSL to 2.3.7
      - Bugfix update (OCSP)
    
    Signed-off-by: Bernard Spil <bernard.spil@hardenedbsd.org>

--- crypto/libressl/ssl/ssl_txt.c
+++ crypto/libressl/ssl/ssl_txt.c
@@ -82,6 +82,7 @@
  * OTHERWISE.
  */
 
+#include <inttypes.h>
 #include <stdio.h>
 
 #include <openssl/buffer.h>
@@ -163,7 +164,7 @@ SSL_SESSION_print(BIO *bp, const SSL_SESSION *x)
 	}
 
 	if (x->time != 0) {
-		if (BIO_printf(bp, "\n    Start Time: %lld", (long long)x->time) <= 0)
+		if (BIO_printf(bp, "\n    Start Time: %"PRId64, (int64_t)x->time) <= 0)
 			goto err;
 	}
 	if (x->timeout != 0L) {
