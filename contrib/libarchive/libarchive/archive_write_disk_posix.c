38df545161cd1ea6b98b1ee6f8bf15d16daf35d3
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2016-09-01 21:49:47 +0200

    HBSD: fix merge conflict contrib/libarchive/libarchive/archive_write_disk_posix.c in after 2e9dcc433f6e69fa78c1e125f795edf8018230b4
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- contrib/libarchive/libarchive/archive_write_disk_posix.c
+++ contrib/libarchive/libarchive/archive_write_disk_posix.c
@@ -2402,21 +2402,8 @@ check_symlinks(struct archive_write_disk *a)
 			/* We've hit a dir that doesn't exist; stop now. */
 			if (errno == ENOENT) {
 				break;
-<<<<<<< HEAD
 			else
 				return (ARCHIVE_FAILED);
-=======
-			} else {
-				/* Note: This effectively disables deep directory
-				 * support when security checks are enabled.
-				 * Otherwise, very long pathnames that trigger
-				 * an error here could evade the sandbox.
-				 * TODO: We could do better, but it would probably
-				 * require merging the symlink checks with the
-				 * deep-directory editing. */
-				return (ARCHIVE_FAILED);
-			}
->>>>>>> origin/freebsd/10-stable/master
 		} else if (S_ISLNK(st.st_mode)) {
 			if (c == '\0') {
 				/*
