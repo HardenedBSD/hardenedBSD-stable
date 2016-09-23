cec1b50069eb7c4b1fe9efe0c4596cafbf63998e
Shawn Webb <shawn.webb@hardenedbsd.org>
2016-09-23 08:21:50 -0400

    HBSD: Support ASLR with CloudABI.
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/compat/cloudabi64/cloudabi64_module.c
+++ sys/compat/cloudabi64/cloudabi64_module.c
@@ -113,7 +113,7 @@ cloudabi64_fixup(register_t **stack_base, struct image_params *imgp)
 		VAL(CLOUDABI_AT_PHNUM, args->phnum),
 		VAL(CLOUDABI_AT_TID, td->td_tid),
 		PTR(CLOUDABI_AT_SYSINFO_EHDR,
-		    imgp->proc->p_sysent->sv_shared_page_base),
+		    imgp->proc->p_shared_page_base),
 #undef VAL
 #undef PTR
 		{ .a_type = CLOUDABI_AT_NULL },
