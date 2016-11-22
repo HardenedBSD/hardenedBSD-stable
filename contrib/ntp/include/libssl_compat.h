2510e11f953f51638a76a5497a444da246d2ddff
Shawn Webb <shawn.webb@hardenedbsd.org>
2016-11-22 18:29:01 -0500

    HBSD: Fix LibreSSL support with ntp
    ntp needs some LibreSSL fixups.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    Submitted-by:	Bernard Spil <bernard.spil@hardenedbsd.org>
    MFC-to:		10-STABLE/LibreSSL
    MFC-to:		11-STABLE/LibreSSL

--- contrib/ntp/include/libssl_compat.h
+++ contrib/ntp/include/libssl_compat.h
@@ -25,7 +25,7 @@
 #include "openssl/rsa.h"
 
 /* ----------------------------------------------------------------- */
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 /* ----------------------------------------------------------------- */
 
 # include <openssl/objects.h>
--- contrib/ntp/libntp/libssl_compat.c
+++ contrib/ntp/libntp/libssl_compat.c
@@ -23,7 +23,7 @@
 #include "ntp_types.h"
 
 /* ----------------------------------------------------------------- */
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 /* ----------------------------------------------------------------- */
 
 #include "libssl_compat.h"
