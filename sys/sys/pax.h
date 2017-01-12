75203821d495b70ea874ce6abaca7132767c8e35
Shawn Webb <shawn.webb@hardenedbsd.org>
2017-01-12 11:43:57 -0500

    HBSD: Bump __HardenedBSD_version to 48 for SafeStack
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	47UL
+#define	__HardenedBSD_version	48UL
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hbsd_features {
