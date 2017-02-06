227b378c8c7bb44da3b3ba1c769385762b35690d
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2017-02-06 22:39:43 +0100

    HBSD: same as in previous commit, the i386_read_exec isn't supported on HardenedBSD
    Restore the pre-322d98e6921174c322eb5ccdb2448fced4e301a0 commit state.
    
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/compat/freebsd32/freebsd32_misc.c
+++ sys/compat/freebsd32/freebsd32_misc.c
@@ -453,20 +453,10 @@ freebsd32_mprotect(struct thread *td, struct freebsd32_mprotect_args *uap)
 {
 	int prot;
 
-<<<<<<< HEAD
-	ap.addr = PTRIN(uap->addr);
-	ap.len = uap->len;
-	ap.prot = uap->prot;
-	return (sys_mprotect(td, &ap));
-=======
 	prot = uap->prot;
-#if defined(__amd64__)
-	if (i386_read_exec && (prot & PROT_READ) != 0)
-		prot |= PROT_EXEC;
-#endif
+
 	return (kern_vm_mprotect(td, (vm_offset_t)PTRIN(uap->addr),
 	    uap->len, prot));
->>>>>>> freebsd/current/master
 }
 
 int
@@ -474,25 +464,10 @@ freebsd32_mmap(struct thread *td, struct freebsd32_mmap_args *uap)
 {
 	int prot;
 
-<<<<<<< HEAD
-	ap.addr = (void *) addr;
-	ap.len = len;
-	ap.prot = prot;
-	ap.flags = flags;
-	ap.fd = fd;
-	ap.pos = pos;
-
-	return (sys_mmap(td, &ap));
-=======
 	prot = uap->prot;
-#if defined(__amd64__)
-	if (i386_read_exec && (prot & PROT_READ))
-		prot |= PROT_EXEC;
-#endif
 
 	return (kern_vm_mmap(td, (vm_offset_t)uap->addr, uap->len, prot,
 	    uap->flags, uap->fd, PAIR32TO64(off_t, uap->pos)));
->>>>>>> freebsd/current/master
 }
 
 #ifdef COMPAT_FREEBSD6
