e3c1234490c4c6afd6a715df5dc37d9579b47e6e
Shawn Webb <shawn.webb@hardenedbsd.org>
2017-05-20 11:34:02 -0400

    HBSD: Resolve merge conflict
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    Sponsored-by:	SoldierX

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -626,17 +626,12 @@ struct proc {
 	pid_t		p_reapsubtree;	/* (e) Pid of the direct child of the
 					       reaper which spawned
 					       our subtree. */
-<<<<<<< HEAD
 	vm_offset_t	p_usrstack;	/* (b) Process stack top. */
 	vm_offset_t	p_psstrings;	/* (b) Process psstrings address. */
 	vm_offset_t	p_timekeep_base;	/* (c) Address of timekeep structure. */
 	vm_offset_t	p_shared_page_base;	/* (c) Address of shared page. */
 	vm_offset_t	p_sigcode_base;	/* (c) Address of sigcode. */
-	u_int		p_xexit;	/* (c) Exit code. */
-	u_int		p_xsig;		/* (c) Stop/kill sig. */
 
-=======
->>>>>>> origin/freebsd/11-stable/master
 /* End area that is copied on creation. */
 #define	p_endcopy	p_xexit
 
