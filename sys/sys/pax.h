6e54fbc9f6e9cf5e188fcfecafe8379dc24a6da8
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2017-09-14 20:29:00 +0200

    HBSD: bump __HardenedBSD_version to 1200055 after hbsdcontrol merge
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	1200054UL
+#define	__HardenedBSD_version	1200055UL
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hbsd_features {
