37565403fa31bc816a59893dc50598e242801371
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2017-09-17 19:58:48 +0200

    HBSD: bump __HardenedBSD_version after LibreSSL merge
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	1100049UL
+#define	__HardenedBSD_version	1100050UL
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hbsd_features {
