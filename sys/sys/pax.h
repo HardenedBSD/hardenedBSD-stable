53597cfa151356ba88df7cd4aa9d643169500f79
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2017-10-17 23:44:50 +0200

    HBSD: bump __HardenedBSD_version to 1100052 after aux vector changes
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- sys/sys/pax.h
+++ sys/sys/pax.h
@@ -32,7 +32,7 @@
 #ifndef	_SYS_PAX_H
 #define	_SYS_PAX_H
 
-#define	__HardenedBSD_version	1100051UL
+#define	__HardenedBSD_version	1100052UL
 
 #if defined(_KERNEL) || defined(_WANT_PRISON)
 struct hbsd_features {
