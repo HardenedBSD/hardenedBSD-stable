146c06ec49d1a6c1a8a192cc4ac77bc1568af5f9
Piotr Kubaj <piotr.kubaj@hardenedbsd.org>
2018-07-16 14:43:10 +0200

    HBSD: And missing bracket to wpa_supplicant's tls_openssl.c
    github-issue: #314
    github-issue: #315
    MFC-to: 11-STABLE
    X-MFC-with: ef3f402b239860e360fd8a21e7ffaa0e402c59af
    Submitted-by: Piotr Kubaj <piotr.kubaj@hardenedbsd.org>
    (cherry picked from commit 8565370dfbd3496aa0218cd0e20d9d9fabc8db2b)
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>
    (cherry picked from commit 91ac682f7ef6d6eaabda5cc195de6a3329f9d40f)
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- contrib/wpa/src/crypto/tls_openssl.c
+++ contrib/wpa/src/crypto/tls_openssl.c
@@ -60,7 +60,7 @@ typedef int stack_index_t;
 
 #if (OPENSSL_VERSION_NUMBER < 0x10100000L || \
     (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L) && \
-    !defined(BORINGSSL_API_VERSION)
+    !defined(BORINGSSL_API_VERSION))
 /*
  * SSL_get_client_random() and SSL_get_server_random() were added in OpenSSL
  * 1.1.0 and newer BoringSSL revisions. Provide compatibility wrappers for
