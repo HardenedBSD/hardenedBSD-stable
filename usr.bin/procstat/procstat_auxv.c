bb71e9889d1362df01c2e5162e84cd7a4fc029c8
Shawn Webb <shawn.webb@hardenedbsd.org>
2018-09-06 19:56:53 -0400

    HBSD: Properly display AT_EHDRFLAGS in procstat(1)
    This commit, candidate for upstreaing to FreeBSD, gives procstat(1) the
    ability to properly show the AT_EHDRFLAGS ELF auxiliary vector.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    Sponsored-by:	SoldierX

--- usr.bin/procstat/procstat_auxv.c
+++ usr.bin/procstat/procstat_auxv.c
@@ -179,6 +179,12 @@ procstat_auxv(struct procstat *procstat, struct kinfo_proc *kipp)
 			    prefix, "AT_TIMEKEEP", auxv[i].a_un.a_ptr);
 			break;
 #endif
+#ifdef AT_EHDRFLAGS
+		case AT_EHDRFLAGS:
+			xo_emit("{dw:/%s}{Lw:/%-16s/%s}{:AT_EHDRFLAGS/%#lx}\n",
+			    prefix, "AT_EHDRFLAGS", (u_long)auxv[i].a_un.a_val);
+			break;
+#endif
 #ifdef AT_PAXFLAGS
 		case AT_PAXFLAGS:
 			xo_emit("{dw:/%s}{Lw:/%-16s/%s}{:AT_PAXFLAGS/%#lx}\n",
